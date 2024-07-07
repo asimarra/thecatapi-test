13c741b7ea48f562f395543a9d0e70c77d22bde1
Angelica Simarra <simarra.angelica@gmail.com>
2024-07-07 11:47:31 -0500

    Unit test for the login module

--- tests/src/infrastructure/repositories/auth.repository.impl.test.ts
+++ tests/src/infrastructure/repositories/auth.repository.impl.test.ts
@@ -1,9 +1,13 @@
 import { AuthDataSource, RegisterUserDTO, UserEntity } from "../../../../src/domain";
+import { LoginUserDTO } from "../../../../src/domain/dtos/auth/login-user.dto";
 import { AuthRepositoryImpl } from "../../../../src/infrastructure";
 
 const userData: RegisterUserDTO = { name: "Angie", email: "aaa@gmail.com", password: "password123" };
 
 class AuthDataSourceImpl implements AuthDataSource {
+    async login(loginUserDTO: LoginUserDTO): Promise<UserEntity> {
+        throw new Error("Method not implemented.");
+    }
     async register(registerUserDTO: RegisterUserDTO): Promise<UserEntity> {
         throw new Error("Method not implemented.")
     }
@@ -19,13 +23,26 @@ describe("AuthRepositoryImpl", () => {
     });
 
     describe("Register method", () => {
-        it("should return the result from authDataSource.register", async () => {
+        it("should register correctly.", async () => {
             const expectedUserEntity: UserEntity = { id: "1", ...userData };
             authDataSource.register = jest.fn().mockResolvedValue(expectedUserEntity);
 
             const result = await authRepository.register(userData);
 
             expect(authDataSource.register).toHaveBeenCalledWith(userData);
+            expect(result).toEqual(expectedUserEntity);
+        });
+    });
+
+    describe("Login method", () => {
+        it("should login correctly.", async () => {
+            const loginUserDTO: LoginUserDTO = { email: userData.email, password: userData.password };
+            const expectedUserEntity: UserEntity = { id: "1", ...userData };
+            authDataSource.login = jest.fn().mockResolvedValue(expectedUserEntity);
+
+            const result = await authRepository.login(loginUserDTO);
+
+            expect(authDataSource.login).toHaveBeenCalledWith(loginUserDTO);
             expect(result).toEqual(expectedUserEntity);
         });
     });
--- tests/src/presentation/auth/controller.test.ts
+++ tests/src/presentation/auth/controller.test.ts
@@ -1,12 +1,16 @@
 import * as httpMock from 'node-mocks-http';
 import { AuthController } from "../../../../src/presentation/auth/controller";
 import { AuthDataSource, RegisterUserDTO, UserEntity } from "../../../../src/domain";
+import { LoginUserDTO } from '../../../../src/domain/dtos/auth/login-user.dto';
 
 let req: any, res: any, next;
 
 const userData: RegisterUserDTO = { name: "Angie", email: "aaa@gmail.com", password: "password123" };
 
 class AuthDataSourceImpl implements AuthDataSource {
+    login(loginUserDTO: LoginUserDTO): Promise<UserEntity> {
+        throw new Error('Method not implemented.');
+    }
     async register(registerUserDTO: RegisterUserDTO): Promise<UserEntity> {
         throw new Error("Method not implemented.")
     }
@@ -53,6 +57,37 @@ describe("AuthController", () => {
             expect(res.statusCode).toBe(200);
             expect(res._getJSONData()).toEqual(registeredUser);
             expect(authRepository.register).toHaveBeenCalledWith(expect.any(RegisterUserDTO));
+        });
+    });
+
+    describe("loginUser", () => {
+        it("should return 400 if invalid request body", async () => {
+            req.body = {};
+            authController.loginUser(req, res);
+
+            expect(res.statusCode).toBe(400);
+            expect(res._getJSONData()).toEqual({ message: expect.any(String) });
+        });
+
+        it("should return 500 if any error occurs", async () => {
+            req.body = { email: userData.email, password: userData.password };
+            authRepository.login = jest.fn().mockRejectedValue(new Error());
+            await authController.loginUser(req, res);
+
+            expect(res.statusCode).toBe(500);
+            expect(res._getJSONData()).toEqual({ message: expect.any(String) });
+        });
+
+        it("should return the logged in user data if login is successful", async () => {
+            req.body = { email: userData.email, password: userData.password };
+            const loggedInUser = { ...userData, id: 1 };
+            authRepository.login = jest.fn().mockResolvedValue(loggedInUser);
+
+            await authController.loginUser(req, res);
+
+            expect(res.statusCode).toBe(200);
+            expect(res._getJSONData()).toEqual(loggedInUser);
+            expect(authRepository.login).toHaveBeenCalledWith(expect.any(LoginUserDTO));
         });
     });
 });
