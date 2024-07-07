5cb5930fc2a4e5bb001bbeba830c8447667fd9e1
Angelica Simarra <simarra.angelica@gmail.com>
2024-07-07 16:29:15 -0500

    Some adjustment in the Middleware logic

--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -6,19 +6,31 @@ import { JwtPayload } from "jsonwebtoken";
 
 export class AuthMiddleware {
     static async validateJWT(req: Request, res: Response, next: NextFunction) {
-        const authorization = req.header('Authorization');
-        if (!authorization) return res.status(401).json({ message: 'Token not provided' });
-        if (!authorization.startsWith('Bearer ')) return res.status(401).json({ message: 'Invalid token' });
+        try {
+            const authorization = req.header('Authorization');
+            if (!authorization) return res.status(401).json({ message: 'Token not provided' });
+            if (!authorization.startsWith('Bearer ')) return res.status(401).json({ message: 'Invalid token' });
 
-        const [_, token = ""] = authorization.split(' ');
-        const payload = await jwtAdapter.validateToken(token) as JwtPayload;
-        if (!payload) return res.status(401).json({ message: 'Invalid token' });
+            const [_, token = ""] = authorization.split(' ');
+            const payload = await jwtAdapter.validateToken(token) as JwtPayload;
+            if (!payload) return res.status(401).json({ message: 'Invalid token' });
 
-        const user = await UserModel.findById(payload.id);
-        if (!user) return res.status(401).json({ message: 'Invalid token' });
+            const user = await UserModel.findById(payload.id);
+            if (!user) return res.status(401).json({ message: 'Invalid token' });
 
-        req.body.user = new UserEntity(user.id, user.name, user.email, user.password);
+            req.body.user = new UserEntity(user.id, user.name, user.email, user.password);
 
-        next();
+            next();
+        } catch (error: any) {
+            console.error(error);
+
+            if (error.name === 'TokenExpiredError') {
+                return res.status(401).json({ error: 'Token expired' });
+            } else {
+                return res.status(401).json({ error: 'Invalid token' });
+            }
+
+            return res.status(500).json({ message: 'Internal server error' });
+        }
     };
 }
